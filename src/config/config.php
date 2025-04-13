26d59f5d6613afcaa7de1e85dca9723035686ed5
vladislavpetrov4311 <petrov_vd_4311@mail.ru>
2025-04-13 18:33:48 +0300

    change

--- src/config/config.php
+++ src/config/config.php
@@ -1,13 +1,14 @@
 <?php
 
+/*
 use Dotenv\Dotenv;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
+$dotenv->load();*/
 
 return [
-    'output_file_path' => $_ENV['OUTPUT_FILE_PATH'],
-    'output_file_name' => $_ENV['OUTPUT_FILE_NAME']
+    'output_file_path' => '/rl-dev/open-rfm-full-list/',
+    'output_file_name' => 'latest.txt'
 ];
